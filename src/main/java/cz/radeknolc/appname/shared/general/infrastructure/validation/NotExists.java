8ee6baf4792360d93f4fd73b4cc0a4bc5cb60eb5
Radek Nolč <radek.nolc@icloud.com>
2023-10-04 19:15:26 +0200

    NotExists validation

---
+++ src/main/java/cz/radeknolc/appname/shared/general/infrastructure/validation/NotExists.java
@@ -0,0 +1,18 @@
+package cz.radeknolc.appname.shared.general.infrastructure.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = NotExistsValidator.class)
+@Documented
+public @interface NotExists {
+    String message() default "NOT_EXISTS";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<?> entity();
+    String column();
+}
