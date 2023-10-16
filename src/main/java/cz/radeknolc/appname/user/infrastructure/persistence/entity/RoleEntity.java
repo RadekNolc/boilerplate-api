fd1a10f2fb5f18899c393eac6a78c33afdbb5979
Radek Nolč <radek.nolc@icloud.com>
2023-10-16 18:35:18 +0200

    Tables columns definition

--- src/main/java/cz/radeknolc/appname/user/infrastructure/persistence/entity/RoleEntity.java
+++ src/main/java/cz/radeknolc/appname/user/infrastructure/persistence/entity/RoleEntity.java
@@ -2,7 +2,10 @@ package cz.radeknolc.appname.user.infrastructure.persistence.entity;
 
 import cz.radeknolc.appname.shared.general.domain.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
@@ -16,5 +19,6 @@ public class RoleEntity implements BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
+    @Column(unique = true, nullable = false)
     private String name;
 }
--- src/main/java/cz/radeknolc/appname/user/infrastructure/persistence/entity/UserEntity.java
+++ src/main/java/cz/radeknolc/appname/user/infrastructure/persistence/entity/UserEntity.java
@@ -3,7 +3,10 @@ package cz.radeknolc.appname.user.infrastructure.persistence.entity;
 import cz.radeknolc.appname.shared.general.domain.entity.AuditedEntity;
 import cz.radeknolc.appname.user.domain.enumeration.Status;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -25,9 +28,11 @@ public class UserEntity implements AuditedEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false, length = 32)
     private String username;
+    @Column(nullable = false, length = 320)
     private String email;
+    @Column(nullable = false)
     private String password;
     @Enumerated(EnumType.STRING)
     private Status status;
